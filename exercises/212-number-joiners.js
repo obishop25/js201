8518dca5b1c81db44910ca545959c7e5c0da6d65
Okera Bishop <obishop25@yahoo.com>
2019-06-29 13:39:43 -0500

    I completed 211 number joiners and it passed the npm run watch test

--- exercises/212-number-joiners.js
+++ exercises/212-number-joiners.js
@@ -4,15 +4,32 @@
 // Examples:
 // numberJoinerWhile(1, 10) --> '1_2_3_4_5_6_7_8_9_10'
 // numberJoinerWhile(12, 14) --> '12_13_14'
-
-
-
+function numberJoinerWhile(start,end){
+    let index = start
+    let total = ""
+    while (index <= end ){
+        if (index < end){
+        total += index +"_"}
+        else { total += index}
+        index = index + 1
+    }
+    return total
+}    
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "numberJoinerFor" which does the same thing as "numberJoinerWhile",
 // except using a "for" loop internally.
 
-
-
+function numberJoinerFor(start,end){
+let total = ""
+for(i = start; i<= end; i++ ) {
+    if (i< end) {
+        total += i+"_"
+            }
+    else {total +=i}
+    index = i + 1 
+    }
+    return total
+    }
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "numberJoinerFancy" which does the same thing as "numberJoinerWhile",
 // except it takes an optional third argument specifying the separator between the numbers.
@@ -21,3 +38,17 @@
 // numberJoinerFancy(1, 10) --> '1_2_3_4_5_6_7_8_9_10'
 // numberJoinerFancy(1, 5, '~') --> 1~2~3~4~5
 // numberJoinerFancy(3, 6, '***BANANAS***') --> 1***BANANAS***2***BANANAS***3
+function numberJoinerFancy (start,end,character){
+    let total =""
+    for (i= start; i<= end; i++){
+        if (i < end && character === undefined){
+            total += i + "_"
+        }
+        else if (i < end){ 
+            total += i + character
+        }
+        else {total += i}
+        index = i + 1
+    }
+    return total 
+}
