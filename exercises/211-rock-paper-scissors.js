17d5929553f4d2ee26391ee5c50b6a92f406dbaa
Okera Bishop <obishop25@yahoo.com>
2019-06-28 21:40:10 -0500

    I completed the rock-paper-scissors exercise and it passed in the npm run watch test

--- exercises/211-rock-paper-scissors.js
+++ exercises/211-rock-paper-scissors.js
@@ -6,3 +6,25 @@
 // rockPaperScissors('rock', 'scissors') --> 'player 1'
 // rockPaperScissors('rock', 'paper') --> 'player 2'
 // rockPaperScissors('paper', 'paper') --> 'draw'
+function rockPaperScissors(player1,player2){
+    if (player1 === 'rock' && player2 === 'scissors'){
+        return 'player 1'
+    }
+    else if (player1 === 'rock' && player2 === 'paper'){
+        return 'player 2'
+    }
+    else if (player1 === 'scissors' && player2 === 'rock') {
+        return 'player 2'
+    }
+    else if (player1 === 'scissors' && player2 === 'paper'){
+        return 'player 1'
+    }
+    else if (player1 === 'paper' && player2 === 'rock'){
+        return 'player 1'
+    }
+    else if (player1 === 'paper' && player2 === 'scissors' ){
+        return 'player 2'
+    }
+    else { return 'draw'}
+
+}
